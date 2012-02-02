f496abe2a11f6079fadbbf3eca81562dd0f09684
Numaan Ashraf <numaan.iitm@gmail.com>
2012-02-02 16:13:05 +0530

    modified control unit

--- controlUnit.v
+++ controlUnit.v
@@ -20,9 +20,9 @@ module controlUnit(
     wire[2:0] alucontrol;
     reg [3:0] state;
     reg IorD;
-    reg AluSrcA;
-    reg [1:0] AlUSrcB;
-    reg Aluop;
+    reg ALUScrA;
+    reg [1:0] ALUScrB;
+    reg ALUOp;
     reg RegDst;
     reg MemToReg;
     reg[1:0] PCSrc;
@@ -31,16 +31,16 @@ module controlUnit(
     reg Branch;
     reg MemWrite;
     reg RegWrite;
-    reg[1:0] aluop;
-    aludec ALUDecoder(.alucontrol(alucontrol),.funct(funct),.aluop(aluop));
+    reg[1:0] ALUOp;
+    aludec ALUDecoder(.alucontrol(alucontrol),.funct(funct),.ALUOp(ALUOp));
     always @(posedge clk)
         case(state)
             FETCH:
         begin
             IorD=0;
-            AluSrcA=0;
-            AlUSrcB=2'b01;
-            Aluop=0;
+            ALUScrA=0;
+            ALUScrB=2'b01;
+            ALUOp=0;
             PCSrc=0;
             IRWrite=1;
             PCWrite=1;
@@ -49,21 +49,22 @@ module controlUnit(
             RegWrite=0;
 
         end
-        DECODE:begin
+        DECODE:
+		begin
             IRWrite=0;
             PCWrite=0;
             Branch=0;
             MemWrite=0;
             RegWrite=0;
-            AluSrcA=0;
-            AlUSrcB=2'b11;
-            Aluop=0;
+            ALUScrA=0;
+            ALUScrB=2'b11;
+            ALUOp=0;
         end
         MEMADR:
         begin
-            AluSrcA=1;
-            AlUSrcB=2'b10;
-            Aluop=0;
+            ALUScrA=1;
+            ALUScrB=2'b10;
+            ALUOp=0;
             IRWrite=0;
             PCWrite=0;
             Branch=0;
@@ -71,30 +72,96 @@ module controlUnit(
             RegWrite=0;
         end
         MEMREAD:
+		begin
+            IRWrite=0;
+            PCWrite=0;
+            Branch=0;
+            MemWrite=0;
+            RegWrite=0;
+			IorD = 1;
+        end
         MEMWRITEBACK:
         begin
             IRWrite=0;
             PCWrite=0;
             Branch=0;
             MemWrite=0;
-            RegWrite=0;
+            RegWrite=1;
+			RegDst=0;
+			MemToReg=1;
         end
         MEMWRITE:
         begin
+            IRWrite=0;
+            PCWrite=0;
+            Branch=0;
+            MemWrite=1;
+            RegWrite=0;
+			IorD=1;
         end
         EXECUTE:
         begin
-            AluSrcA=1;
-            AlUSrcB=2'b00;
-            Aluop=2'b10;
+            IRWrite=0;
+            PCWrite=0;
+            Branch=0;
+            MemWrite=0;
+            RegWrite=0;
+			ALUScrA=1;
+			ALUScrB=0;
+			ALUOp=2'b10;
         end
         ALUWRITEBACK:
         begin
-            RegDst=1;
-            MemToReg=0;
+            IRWrite=0;
+            PCWrite=0;
+            Branch=0;
+            MemWrite=0;
             RegWrite=1;
+			RegDst=1;
+			MemToReg=0;
         end
-
+		BRANCH:
+		begin
+            IRWrite=0;
+            PCWrite=0;
+            Branch=1;
+            MemWrite=0;
+            RegWrite=0;
+			ALUScrA=1;
+			ALUScrB=0;
+			ALUOp=2'b01;
+			PCSrc=2'b01;
+        end
+		ADDIEXECUTE:
+		begin
+            IRWrite=0;
+            PCWrite=0;
+            Branch=0;
+            MemWrite=0;
+            RegWrite=0;
+			ALUScrA=1;
+			ALUScrB=2'b10;
+			ALUOp=0;
+        end
+		ADDIWRITEBACK:
+		begin
+            IRWrite=0;
+            PCWrite=0;
+            Branch=0;
+            MemWrite=0;
+            RegWrite=1;
+			RegDst=0;
+			MemToReg=0;
+        end
+		JUMP:
+		begin
+            IRWrite=0;
+            PCWrite=1;
+            Branch=0;
+            MemWrite=0;
+            RegWrite=0;
+			PCSrc=2'b10;
+        end
 
     endcase
     endmodule
