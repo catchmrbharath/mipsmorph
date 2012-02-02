41551fa5b6f17303703ba14ff89bb64c58fd8c25
Bharath M R <catchmrbharath@gmail.com>
2012-02-02 13:17:16 +0530

    typo on bitwidth for output addresses

--- regfile.v
+++ regfile.v
@@ -1,12 +1,13 @@
 `define imwidth 16
 `define adwidth 5
 `define datawidth 32
-module regfile(
-    output[`imwidth-1:0] rd1,rd2,
+module regfile #(parameter MEM=32)
+(
+    output[`datawidth-1:0] rd1,rd2,
     input [`adwidth-1:0] a1,a2,a3,
     input [`datawidth-1:0] wr,
     input clk,wrenable);
-    reg[`datawidth-1:0] registers[31:0];
+    reg[`datawidth-1:0] registers[MEM-1:0];
     assign rd1 = registers[a1];
     assign rd2 = registers[a2];
     always @ (posedge clk)
