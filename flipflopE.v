463d2c3686ae4647cf38c0952ccfd0fbed516cd4
Bharath M R <catchmrbharath@gmail.com>
2012-02-02 13:12:51 +0530

    added flipflop with enable

---
+++ flipflopE.v
@@ -0,0 +1,12 @@
+module flipflopE #(parameter WIDTH=32)
+    (output reg [ WIDTH-1:0] q,
+        input [WIDTH-1:0] d,
+        input clk,reset,enable);
+    always @(posedge clk or reset or enable)
+        if(reset)
+            q<=0;
+        else if(enable)
+            q<=d;
+        else
+            q<=q;
+        endmodule
