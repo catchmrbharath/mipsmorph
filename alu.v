ed6e5428b031e59a6f7b4ebd2a0992505617ede2
Bharath M R <catchmrbharath@gmail.com>
2012-02-02 13:13:24 +0530

    alu added

---
+++ alu.v
@@ -0,0 +1,25 @@
+module alu(
+    output reg [31:0] ALUout,
+    output reg zeroFlag,
+    input[31:0] srcA,srcB,
+    input[2:0] aluControl);
+
+always @(*)
+begin
+    case(aluControl)
+        3'b010: ALUout=srcB+srcA;
+        3'b110: ALUout=srcA-srcB;
+        3'b000: ALUout=srcB & srcA;
+        3'b001: ALUout=srcA | srcB;
+        3'b111: ALUout= (srcA<srcB) ? 1:0;
+        3'bxxx: ALUout=0; //TODO:think of something else here
+    endcase
+    if(ALUout==0)
+        zeroFlag=1;
+    else
+        zeroFlag=0;
+end
+endmodule
+
+
+
