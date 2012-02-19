7c440adc0aa8296c0f03f048e04da6f4d418a947
Bharath M R <catchmrbharath@gmail.com>
2012-02-19 13:13:05 +0530

    read changed from combinational to read on negedge. takes care of read after write problems

--- regfile.v
+++ regfile.v
@@ -2,19 +2,33 @@
 `define datawidth 32
 module regfile #(parameter MEM=32)
 (
-    output[`datawidth-1:0] rd1,rd2,
+    output reg [`datawidth-1:0] rd1,rd2,
     input [`adwidth-1:0] a1,a2,a3,
     input [`datawidth-1:0] wr,
     input clk,wrenable);
 
     reg[`datawidth-1:0] registers[MEM-1:0];
     initial
+    begin
         registers[0]=0;
-    assign rd1 = registers[a1];
-    assign rd2 = registers[a2];
+        registers[1]=0;
+        registers[2]=0;
+        registers[3]=0;
+    end
+    always @ (negedge clk)
+    begin
+        rd1 = registers[a1];
+        rd2 = registers[a2];
+        $display($time,,,,"wrenable = %d registers[2] = %d a1 = %d",wrenable,rd1,a1);
+    end
     always @ (posedge clk)
+    begin
+        #1;
         if(wrenable) 
 			registers[a3]<=wr;
+        else
+            registers[a3]<=registers[a3];
+    end
 endmodule
 
         
