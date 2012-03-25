0c91aed1d3945ef4c838aa1d1799be656a0fb519
Numaan Ashraf <numaan.iitm@gmail.com>
2012-03-25 22:16:17 +0530

    modified controller

--- aludec.v
+++ aludec.v
@@ -1,6 +1,6 @@
 module aludec(
     output reg[2:0] alucontrol,
-    input [5:0] funct,
+    input [2:0] funct,
     input [1:0] aluop);
     
 always @(*)
@@ -8,11 +8,10 @@ always @(*)
         2'b00: alucontrol<= 3'b010; //add
         2'b01: alucontrol<= 3'b110; // sub
         default: case(funct)
-            6'b100000: alucontrol<=3'b010; //add
-            6'b100010: alucontrol<=3'b110; //sub
-            6'b100100: alucontrol<=3'b000; // AND
-            6'b100101: alucontrol<=3'b001; //OR
-            6'b101010: alucontrol<=3'b111; //SLT
+            3'b000: alucontrol<=3'b010; //add
+            3'b010: alucontrol<=3'b110; //sub
+            3'b100: alucontrol<=3'b000; // AND
+            3'b101: alucontrol<=3'b001; //OR
             default: alucontrol<=3'bxxx;
         endcase
     endcase
--- maindecoder.v
+++ maindecoder.v
@@ -7,7 +7,7 @@ module maindec (
 	output regwrite,
 	output jump,
 	output [1:0] aluop,
-	input [5:0] op);
+	input [3:0] op);
 	
 	reg [8:0] controls;
 	
@@ -15,12 +15,12 @@ module maindec (
 	
 	always @ (*)
 		case(op)
-			6'b000000: controls <= 9'b110000010; // R-Type
-			6'b100011: controls <= 9'b101001000; // LW
-			6'b101011: controls <= 9'b001010000; // SW
-			6'b000100: controls <= 9'b000100001; // BEQ
-			6'b001000: controls <= 9'b101000000; // ADDI
-			6'b000010: controls <= 9'b000000100; // J
+			4'b0000: controls <= 9'b110000010; // R-Type
+			4'b0100: controls <= 9'b101000000; // ADDI
+			4'b1011: controls <= 9'b101001000; // LW
+			4'b1111: controls <= 9'b001010000; // SW
+			4'b1000: controls <= 9'b000100001; // BEQ
+			4'b0010: controls <= 9'b000000100; // J
 			default: controls <= 9'bxxxxxxxxx; // default case
 		endcase
 endmodule
