3fa55853e3b5f4d430e07e84800584addeca33b5
Bharath M R <catchmrbharath@gmail.com>
2012-03-25 22:03:36 +0530

    changed the size of the instr mem

--- instr.v
+++ instr.v
@@ -1,10 +1,10 @@
-`define size 31
+`define size 16
 module instr #(parameter MEM=18)
 (
-    output [`size:0] out,
+    output [`size-1:0] out,
     input [5:0] address );
 
-    reg [`size:0] mem[MEM-1:0];
+    reg [`size-1:0] mem[MEM-1:0];
     initial
     begin
         $readmemh("instruction.txt",mem);
